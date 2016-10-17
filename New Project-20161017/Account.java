3ffffac38936a27f6bcdd87502e1076333829080
RomanAlaivi <romanalaivi@gmail.com>
2016-10-17 16:58:42 -0600

    Created by Coding Ground Client

---
+++ New Project-20161017/Account.java
@@ -0,0 +1,9 @@
+public class Account {
+    public Account (String number, double balance, String customerName, String customerEmailAddress, String customerPhoneNumber){
+    this.number=number;
+    this.balance=balance;
+    this.customerName=customerName;
+    this.customerEmailAddress=custoemrEmailAddress;
+    this.customerPhoneNum=customerPhoneNumber;
+    }
+}
